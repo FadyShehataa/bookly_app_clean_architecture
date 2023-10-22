4d3b905a96719d88e9679816472a278b54e5d2ef
FadyShehataa <fadyshehata40@gmail.com>
2023-10-22 18:35:56 +0300

    listen to scroll list

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,13 +1,28 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../manager/featured_books_cubit/featured_books_cubit.dart';
 import 'custom_book_item.dart';
 
-class FeaturedBooksListView extends StatelessWidget {
+class FeaturedBooksListView extends StatefulWidget {
   const FeaturedBooksListView({Key? key, required this.books})
       : super(key: key);
 
   final List<BookEntity> books;
+
+  @override
+  State<StatefulWidget> createState() => _FeaturedBooksListViewState();
+}
+
+class _FeaturedBooksListViewState extends State<FeaturedBooksListView> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +33,27 @@ class FeaturedBooksListView extends StatelessWidget {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: CustomBookImage(image: books[index].image ?? ''),
+            child: CustomBookImage(image: widget.books[index].image ?? ''),
           );
         },
-        itemCount: books.length,
+        itemCount: widget.books.length,
+        controller: _scrollController,
       ),
     );
+  }
+
+  void _scrollListener() {
+    var currentPosition = _scrollController.position.pixels;
+    var maxScrollLength = _scrollController.position.maxScrollExtent;
+    if (currentPosition >= 0.7 * maxScrollLength) {
+      BlocProvider.of<FeaturedBooksCubit>(context).fetchFeaturedBooks();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
   }
 }
