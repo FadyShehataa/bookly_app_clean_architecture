b818b67a331e75573bba405580638a84afd79f05
FadyShehataa <fadyshehata40@gmail.com>
2023-10-22 11:51:54 +0300

    implement newest books cubit & states

---
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../../domain/entities/book_entity.dart';
+import '../../../domain/use_cases/fetch_newest_books_use_case.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+
+    var result = await fetchNewestBooksUseCase.call();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(errMessage: failure.errMessage)),
+      (books) => emit(NewestBooksSuccess(books: books)),
+    );
+  }
+}
